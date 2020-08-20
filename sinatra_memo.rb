7a335121d59424c741b347877c5e85aad3680450
tam03 <tamuramura2803@gmail.com>
2020-08-20 15:32:49 +0900

    add new file

---
+++ sinatra_memo.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# #くじプログラム
+# require "sinatra"
+# get "/kuji" do
+#     ["大吉", "中吉", "末吉", "凶"].sample
+# end
+
+# アクセスするプログラム
+require 'sinatra'
+get '/hi' do
+  'hi!'
+end
+# #ドリンク
+# require "sinatra"
+# get "/drink" do
+#     ["カフェ", "モカ", "コーヒー"].sample
+# end
