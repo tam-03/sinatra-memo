44fc11217d2be9d3aa9130179ff65399ae5e18d7
tam03 <tamuramura2803@gmail.com>
2020-08-21 10:14:12 +0900

    sinatra practice

--- sinatra_memo.rb
+++ sinatra_memo.rb
@@ -1,1 +1,25 @@
 # frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  'how are you?'
+end
+
+get '/path/to' do
+  "this is [/path/to]"
+end
+
+get '/hello/*' do |name|
+  "hello #{name}. how are you?"
+end
+
+get '/erb_template_page' do
+  erb :erb_template_page
+end
+
+get '/markdown_template_page' do
+  markdown :markdown_template_page
+end
+
