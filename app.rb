5a712e47b55aed1f96e6d132c376a920d9e1778f
tam03 <tamuramura2803@gmail.com>
2020-08-25 18:34:47 +0900

    prototype

--- app.rb
+++ app.rb
@@ -1,1 +1,58 @@
 # frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'fileutils'
+
+enable :method_override
+
+before do
+  @memo = Dir.glob("*").grep(/.txt/).map do |t| t end
+end
+
+get '/' do
+  @title = "My Memo"
+  @app_name = "メモアプリ"
+  erb :index
+end
+
+get '/create' do
+    erb :create
+end
+
+post '/create' do
+    @memo_title = params[:memo_title]
+    @memo_content = params[:memo_content]
+    File.open("#{@memo_title}.txt", "w") do |f|
+      f.puts("#{@memo_title},#{@memo_content}")
+    end
+    erb :save
+end
+
+get '/show/:txt_name' do |t|
+    @txt_name = t
+  erb :show
+end
+
+delete '/show/:txt_name' do |t|
+  File.delete("#{t}")
+  redirect to('/')
+end
+
+get '/edit/:txt_name' do |t|
+  @txt_name = t
+    File.open("#{t}", "r") do |f|
+        @memo_date = f.read.split(",")
+    end
+  erb :edit
+end
+
+patch '/edit/:txt_name' do |t|
+  @txt_name = t
+  File.open("#{@txt_name}", "w") do |f|
+    f.puts("#{params[:edit_title]},#{params[:edit_content]}")
+  end
+  File.rename("#{@txt_name}", "#{params[:edit_title]}.txt")
+  redirect to('/')
+end
+
