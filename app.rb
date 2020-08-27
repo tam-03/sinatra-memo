852c1ddbaac524f58d6fd696d6d79faffa76a73d
tam03 <tamuramura2803@gmail.com>
2020-08-27 12:32:57 +0900

    rubocop app.rb

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 enable :method_override
 
 before do
-  @memo = Dir.glob("*").grep(/.txt/).map do |t| t end
+  @memo = Dir.glob('*').grep(/.txt/).map { |t| t }
 end
 
 get '/' do
@@ -18,38 +18,37 @@ get '/create' do
 end
 
 post '/create' do
-    @memo_title = params[:memo_title]
-    @memo_content = params[:memo_content]
-    File.open("#{@memo_title}.txt", "w") do |f|
-      f.puts("#{@memo_title},#{@memo_content}")
-    end
-    erb :save
+  @memo_title = params[:memo_title]
+  @memo_content = params[:memo_content]
+  File.open("#{@memo_title}.txt", 'w') do |f|
+    f.puts("#{@memo_title},#{@memo_content}")
+  end
+  erb :save
 end
 
 get '/show-:txt_name' do |t|
-    @txt_name = t
+  @txt_name = t
   erb :show
 end
 
 delete '/show-:txt_name' do |t|
-  File.delete("#{t}")
+  File.delete(t.to_s)
   redirect to('/')
 end
 
 get '/edit-:txt_name' do |t|
   @txt_name = t
-    File.open("#{t}", "r") do |f|
-        @memo_date = f.read.split(",")
-    end
+  File.open(t.to_s, 'r') do |f|
+    @memo_date = f.read.split(',')
+  end
   erb :edit
 end
 
 patch '/edit-:txt_name' do |t|
   @txt_name = t
-  File.open("#{@txt_name}", "w") do |f|
+  File.open(@txt_name.to_s, 'w') do |f|
     f.puts("#{params[:edit_title]},#{params[:edit_content]}")
   end
-  File.rename("#{@txt_name}", "#{params[:edit_title]}.txt")
+  File.rename(@txt_name.to_s, "#{params[:edit_title]}.txt")
   redirect to('/')
 end
-
